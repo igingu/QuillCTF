4f965a22c266e261042f52f81faece34c1a3b4b6
Ionut <ionut.gingu@gmail.com>
2023-02-06 09:01:01 +0200

    Foundry initial setup and changing challenges folder.

--- src/weth-10/weth-10.sol
+++ src/weth-10/weth-10.sol
@@ -29,37 +29,17 @@ contract WETH10 is ERC20("Messi Wrapped Ether", "WETH10"), ReentrancyGuard {
     }
 
     /// @notice Request a flash loan in ETH
-    function execute(address receiver, uint256 amount, bytes calldata data) external nonReentrant {
+    function execute(
+        address receiver,
+        uint256 amount,
+        bytes calldata data
+    ) external nonReentrant {
         uint256 prevBalance = address(this).balance;
         Address.functionCallWithValue(receiver, data, amount);
 
-        require(address(this).balance >= prevBalance, "flash loan not returned");
-    }
-}
-
-contract Weth10Test is Test {
-    WETH10 public weth;
-    address owner;
-    address bob;
-
-    function setUp() public {
-        weth = new WETH10();
-        bob = makeAddr("bob");
-
-        vm.deal(address(weth), 10 ether);
-        vm.deal(address(bob), 1 ether);
-    }
-
-    function testHack() public {
-        assertEq(address(weth).balance, 10 ether, "weth contract should have 10 ether");
-
-
-        vm.startPrank(bob);
-
-        // hack time!
-
-        vm.stopPrank();
-        assertEq(address(weth).balance, 0, "empty weth contract");
-        assertEq(bob.balance, 11 ether, "player should end with 11 ether");
+        require(
+            address(this).balance >= prevBalance,
+            "flash loan not returned"
+        );
     }
 }
